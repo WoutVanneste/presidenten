7b507d53fe4a0af736b872df6b1eddf83f494a19
WoutVanneste99 <wout.vanneste@delaware.pro>
2023-07-31 09:10:18 +0200

    move assets to src folder and set correct welcome message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/harten-K.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Presidenten applicatie in progressie...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Kom later terug om te presidenten!
+        </p>
       </header>
     </div>
   );
